c23d5ca959f49d67b49c586a02677d375ab8fe3b
Vakati Sandeep <vakatisandeep82@gmail.com>
2026-04-28 05:14:00 +0530

    Update home route message for successful deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello from Flask**! 🚀</h1><p>Deployed via GitHub Actions CI/CD</p>"
+    return "<h1>Hello from Flask**! 🚀</h1><p>Deployed via GitHub Actions CI/CD successfully</p>"
 
 @app.route("/health")
 def health():
